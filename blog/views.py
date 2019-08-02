85381ff7e936cea4e5c32b65059e9acf1370e5c5
caprize <iskan20022@mail.ru>
2019-08-02 13:35:16 +0300

    dadaya

--- blog/views.py
+++ blog/views.py
@@ -10,13 +10,8 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
-import telebot
-from telebot.types import Message
 import requests
-from telebot import types
-BASE_URL='https://api.telegram.org/bot/731947153:AAETaq49IdPhGCg9YssRF6RmW3ZIjzAdX4'
-TOKEN = '731947153:AAETaq49IdPhGCg9YssRF6RmW3ZIjzAdX4o'
-tb = telebot.TeleBot(TOKEN)
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
